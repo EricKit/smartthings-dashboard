3712081c8a7b23a4b438e37bb133a83b81afb92d
Eric Kitaif <eric.kitaif@gmail.com>
2021-12-20 09:36:12 -0500

    Added outside temp

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -48,6 +48,16 @@
 					</dd>
 				</div>
 			</dl>
+			<h3 class="text-lg leading-6 font-medium text-sky-900 mt-8">Outside</h3>
+			<dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
+				<div class="px-4 py-5 bg-white shadow rounded-lg overflow-hidden sm:p-6">
+					<dt class="text-sm font-medium text-sky-500 truncate">Temperature</dt>
+					<dd class="mt-1 text-3xl font-semibold text-sky-900">
+						{outsideTemperature[outsideTemperature.length - 1]?.value +
+							outsideTemperature[outsideTemperature.length - 1]?.unit}
+					</dd>
+				</div>
+			</dl>
 		</div>
 		<div class="p-8">
 			<Chart leftData={temperature} leftDataTwo={outsideTemperature} rightData={humidity} />
